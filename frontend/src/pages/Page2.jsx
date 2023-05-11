7c25040de6215a4e4a12f8a5702f54a48c433ea0
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-05-11 18:40:35 +0200

    cssPage2

--- frontend/src/pages/Page2.jsx
+++ frontend/src/pages/Page2.jsx
@@ -11,29 +11,31 @@ function Page2() {
       <div className="imgPage2">
         <img
           src="https://media.discordapp.net/attachments/1106113841487691776/1106160974408466442/TourDePise.jpg?width=1089&height=817"
-          alt="Tour de Pise"
-          class="img-fluid containImgPage2"
+          alt="La Tour de Pise"
+          className="img-fluid containImgPage2"
         />
       </div>
-      <div class="card cardPage2">
+      <div className="card cardPage2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nemo aut
         id, ullam obcaecati architecto fugiat tempore sint doloremque quam
         aperiam enim in fugit, quidem, aspernatur neque voluptates quod
         temporibus!
       </div>
-      <div class="card cardPage2">
+      <div className="card cardPage2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nemo aut
         id, ullam obcaecati architecto fugiat tempore sint doloremque quam
         aperiam enim in fugit, quidem, aspernatur neque voluptates quod
         temporibus!
       </div>
-      <div class="card cardPage2">
+      <div className="card cardPage2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nemo aut
         id, ullam obcaecati architecto fugiat tempore sint doloremque quam
         aperiam enim in fugit, quidem, aspernatur neque voluptates quod
         temporibus!
       </div>
-      <button className="css-button-retro">Valide ta Quête</button>
+      <button type="button" className="css-button-retro">
+        Valide ta Quête
+      </button>
     </div>
   );
 }
