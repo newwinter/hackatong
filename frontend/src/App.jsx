b162191ba3c7d5f4f9a9b26c11668e4254fb6c02
NicolasPanis <nicolaspanis12@gmail.com>
2023-05-11 22:16:34 +0200

    Création de la navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,25 @@ import "./App.css";
 import Page1 from "@pages/Page1";
 import Page2 from "@pages/Page2";
 import Page3 from "@pages/Page3";
+import About from "@pages/About";
+import Contact from "@pages/Contact";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
+import NavBar from "@components/NavBar";
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+        <NavBar />
       </div>
+      <div></div>
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/Page2" element={<Page2 />} />
         <Route path="/Page3" element={<Page3 />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
