1ed758b46d180352f979a5598f02d91550ec1ed4
NicolasPanis <nicolaspanis12@gmail.com>
2023-05-11 22:33:21 +0200

    Fix navbar again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Page1 from "@pages/Page1";
 import Page2 from "@pages/Page2";
-
 import About from "@pages/About";
 import Contact from "@pages/Contact";
 import Passport from "@pages/PassPort";
@@ -20,7 +19,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/Page2" element={<Page2 />} />
-        <Route path="/Page3" element={<Page3 />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Passport" element={<Passport />} />
